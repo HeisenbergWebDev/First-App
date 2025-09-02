1a51d0f6e62fb60e4030d6f92fd35bb933cea8e2
HeisenbergWebDev <heisenbergwebdev@gmail.com>
2025-09-02 10:09:17 +0200

    wokrking on home screen

--- 00-starting-project/screens/StartGameScreen.js
+++ 00-starting-project/screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from '../components/PrimaryButton';
 const StartGameScreen = () => {
   return ( 
   <View style={styles.inputContainer}>
-    <TextInput />
+    <TextInput style={styles.numberInput} maxLength={2} keyboardType='numberpad' />
     <PrimaryButton> reset</PrimaryButton>
     <PrimaryButton> confirm </PrimaryButton>
   </View>    
@@ -26,6 +26,18 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
         shadowOpacity: 0.25,
-    
         },
+    numberInput: {
+        height: 50,
+        width: 50,
+        textAlign: 'center',
+        fontSize: 24,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+       
+
+    }
 });
